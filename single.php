7f395b461923779deb344135f2655b27293e4989
Jordan Lambrecht <jordan@pixelbakery.co>
2021-04-23 15:13:12 -0500

    Updated margins

--- single.php
+++ single.php
@@ -17,35 +17,31 @@ get_header();
 			the_post();
 
 			get_template_part( 'template-parts/content', get_post_type() );?>
+			<section class="container-fluid my-0">
+				<?php
+				$directory = esc_url( get_stylesheet_directory_uri());
+				$leftArrow = $directory . '/assets/icons/mmh_arrowLeft_white.svg';
+				$rightArrow = $directory . '/assets/icons/mmh_arrowRight_white.svg';
+				$leftPost = '<i><img class="arrow-left d-inline-block" src="' . $leftArrow . '"  height="16px"/></i> <strong>%link</strong>';
+				$rightPost = '<strong>%link</strong><i><img class="arrow-left d-inline-block" src="' . $rightArrow . '"  height="16px"/></i>'
+				?>
+				<div class="container px-5">
+					<div class="row py-3">
+						<div class="col-6 text-left align-middle d-flex flex-row align-content-center other-articles">
 
-			<section class="continer-fluid bg-offWhite" id="postNavigation">
-				<div class="container">
-					<div class="row">
-						<div class="col-12">
-							<div class="col-6 text-left">
-								<?php previous_post_link( '<------- <strong>%link</strong>' ); ?>
-							</div>
-							<div class="col-6 text-right">
-								<?php next_post_link( '<strong>%link</strong> ------->' ); ?>
-							</div>
-							<?php
-							// if(! empty previous_post_link()):
-							//
-							// endif;
 
-							// the_post_navigation(
-							// 	array(
-							// 		'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'mmhf' ) . '</span> <span class="nav-title">%title</span>',
-							// 		'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'mmhf' ) . '</span> <span class="nav-title">%title</span>',
-							// 	)
-							// );
-							?>
+							<?php previous_post_link( $leftPost ); ?>
+						</div>
+						<div class="col-6 text-right align-middle  d-flex flex-row align-content-center">
+							<?php next_post_link( $rightPost ); ?>
 						</div>
 					</div>
+				</div>
 
-				</div>
-				</div>
 			</section>
+
+
+
 
 
 			<?php
